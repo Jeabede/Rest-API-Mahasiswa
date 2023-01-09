5f8b12b95c19453b4bd0709a8ccaf33824d96853
Jaihan Abidin <51147214+Jeabede@users.noreply.github.com>
2023-01-09 17:10:20 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,25 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa"
+  "FLUTTER_TARGET=E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Myserver\\web\\public\\flutter\\aplikasi_mahasiswa\\.dart_tool\\package_config.json"
+)
